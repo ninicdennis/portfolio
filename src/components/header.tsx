aa29c615e3e0ca645de6f833bd26450e5fcc6850
ninicdennis <pupperbot@protonmail.com>
2023-03-16 21:47:56 -0600

    feat: used localStorage theme to store which active theam version is currently on

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MdDarkMode } from 'react-icons/md';
 import { FaSun } from 'react-icons/fa';
 import Link from 'next/link';
@@ -6,34 +6,26 @@ import Link from 'next/link';
 const Header = () => {
 	const [check, setCheck] = useState(false);
 
-	const changeDark = () => setCheck(false);
-	const changeLight = () => setCheck(true);
+	useEffect(() => {
+		const theme = localStorage.getItem('theme');
+		if (theme === 'corporate') setCheck(true);
+	}, []);
+
 	return (
 		<div>
 			<div className='navbar bg-base-300 justify-between'>
 				<Link className='btn btn-ghost normal-case text-xl' href='/'>
 					Dennis Ninic
 				</Link>
-				{check ? (
-					<button
-						className='btn btn-square'
-						data-set-theme='dracula'
-						data-act-class='ACTIVECLASS'
-						onClick={changeDark}
-					>
-						<MdDarkMode size={32} />
-					</button>
-				) : (
-					<button
-						aria-label='Theme Toggle'
-						className='btn btn-square'
-						data-set-theme='corporate'
-						data-act-class='ACTIVECLASS'
-						onClick={changeLight}
-					>
-						<FaSun size={32} />
-					</button>
-				)}
+				<button
+					aria-label='Theme Toggle'
+					className='btn btn-square'
+					data-set-theme={check ? 'dracula' : 'corporate'}
+					data-act-class='ACTIVECLASS'
+					onClick={() => setCheck(check ? false : true)}
+				>
+					{check ? <MdDarkMode size={32} /> : <FaSun size={32} />}
+				</button>
 			</div>
 		</div>
 	);
